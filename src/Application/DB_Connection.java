1e15ccb95124573edc0e8c4edff0fc780d34b9ff
shreyashbhatt1996 <shreyash.bhatt19@gmail.com>
2019-12-20 13:54:10 +0530

    Optimised DB_Connection,UserSession

--- src/Application/DB_Connection.java
+++ src/Application/DB_Connection.java
@@ -3,13 +3,17 @@ package Application;
 import java.sql.*;
 
 public class DB_Connection {
-    Connection conn = null;
+    private Connection conn = null;
+    public Connection getConenctionInstance(){
+        if(conn == null) return connectdb();
+        else return conn;
+    }
     public static Connection connectdb()
     {
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/face_recognition","root","28021413");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/face_recognition","root","28021413");
             return conn;
         }
         catch(Exception e)
